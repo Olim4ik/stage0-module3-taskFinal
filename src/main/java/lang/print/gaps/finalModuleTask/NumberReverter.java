6633b162449742708a96d40fe2a89a842351d39d
Olim4ik <oakhmadjonov.uz@gmail.com>
2023-12-06 12:47:11 +0500

    test 1

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        System.out.print(number % 10);
+        number /= (int) 10;
+        System.out.print(number % 10);
+        number /= (int) 10;
+        System.out.println(number % 10);
     }
 }
